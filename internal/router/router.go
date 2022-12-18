dcd17c3ba4e1e5eb79a526f67b698947e22c8507
Elie CHARRA <e1ie@protonmail.com>
2022-12-18 19:22:22 +0100

    feat: redirect people to gh on GET /

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 
 	"github.com/estrys/estrys/internal/activitypub/routes"
 	"github.com/estrys/estrys/internal/domain/status"
@@ -10,6 +11,10 @@ import (
 func GetRouter() *mux.Router {
 	r := mux.NewRouter()
 	routes.Router(r)
+	r.Path("/").HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
+		responseWriter.Header().Add("Location", "https://github.com/estrys/estrys")
+		responseWriter.WriteHeader(http.StatusFound)
+	})
 	status.StatusRouter(r)
 	return r
 }
